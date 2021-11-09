4ea17d518c0f3f30e4678ecf2a36d1d6474cee8b
pattyqr <patricia.quevedo@unmsm.edu.pe>
2021-11-09 01:18:28 -0500

    direccion

---
+++ data/direccion_1.js
@@ -0,0 +1,8 @@
+var json_direccion_1 = {
+"type": "FeatureCollection",
+"name": "direccion_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ -77.015749288137897, -12.140137609763915 ] } }
+]
+}
---
+++ data/estaciones_3.js
@@ -0,0 +1,9 @@
+var json_estaciones_3 = {
+"type": "FeatureCollection",
+"name": "estaciones_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "nombre_est": "Estación Balta" }, "geometry": { "type": "Point", "coordinates": [ -77.017863498340446, -12.141893795492468 ] } },
+{ "type": "Feature", "properties": { "id": null, "nombre_est": "Estación Plaza Flores" }, "geometry": { "type": "Point", "coordinates": [ -77.019302794208926, -12.135028109094655 ] } }
+]
+}
